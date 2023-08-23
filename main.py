b0db931c4f9d7f47d8b4524a4ef864dbadb6084b
colabacc77 <142879124+colabacc77@users.noreply.github.com>
2023-08-23 16:40:54 +0300

    add test form

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import requests
-from flask import Flask, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-  response = requests.get('http://jsonip.com')
-  return [response.status_code, response.text]
+  return render_template('form.html')
 
+@app.route('/form', methods=['POST'])
+def quiz_answers():
+    q1 = request.form['q1']
+    q2 = request.form['q2']
+    q4 = request.form['q1']
+    q5 = request.form['q2']
+    response = requests.get('http://jsonip.com')
+    return [response.status_code, response.text, q1, q2, q4, q5]
+  
 if __name__ == "__main__":
     home()
     app.run(host="0.0.0.0", port=8080)
